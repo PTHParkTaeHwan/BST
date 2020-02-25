4229aebc854f2ad4b44d88c5a49e18be42e1bb6f
PTHParkTaeHwan <pth0810@naver.com>
2020-02-25 17:38:47 +0900

    temp

--- BST/main.cpp
+++ BST/main.cpp
@@ -1,6 +1,105 @@
+//2ÁøÆ®¸®
 #include<iostream>
+using namespace std;
+
+struct TreeNode
+{
+	int Value;
+	TreeNode *left, *right;
+
+	TreeNode(int value) : Value(value), left(NULL), right(NULL) {}
+};
+
+class Tree
+{
+public:
+	Tree() : root(NULL) {}
+
+public:
+	void Insert(int value)
+	{
+		Insert(root, value);
+	}
+	void Remove(int value)
+	{
+
+	}
+	void InOrder()
+	{
+		InOrder(root);
+	}
+	void PostOrder()
+	{
+		PostOrder(root);
+	}
+	void PreOrder()
+	{
+		PreOrder(root);
+	}
+
+private:
+	void Insert(TreeNode* &node, int value)
+	{
+		if (node == NULL) node = new TreeNode(value);
+		else if (value < node->Value) Insert(node->left, value);
+		else if (value > node->Value) Insert(node->right, value);
+	}
+	void InOrder(TreeNode* node)
+	{
+		if (node != NULL)
+		{
+			InOrder(node->left);
+			cout << node->Value << " ";
+
+			InOrder(node->right);
+		}
+	}
+	void PostOrder(TreeNode* node)
+	{
+		if (node != NULL)
+		{
+			PostOrder(node->left);
+			PostOrder(node->right);
+			cout << node->Value << " ";
+		}
+	}
+	void PreOrder(TreeNode* node)
+	{
+		if (node != NULL)
+		{
+			cout << node->Value << " ";
+			PreOrder(node->left);
+			PreOrder(node->right);
+		}
+	}
+
+private:
+	TreeNode * root;
+};
 
 int main()
 {
+	Tree tr;
+	tr.Insert(10);
+	tr.Insert(20);
+	tr.Insert(30);
+	tr.Insert(40);
+	tr.Insert(50);
+	tr.Insert(60);
+	tr.Insert(70);
+
+	cout << "inOrder" << endl;
+	tr.InOrder();	
+	cout << endl << endl;
+
+	cout << "PostOrder" << endl;
+	tr.PostOrder();
+	cout << endl << endl;
+
+	cout << "PreOrder" << endl;
+	tr.PreOrder();
+	cout << endl << endl;
+
+
 	return 0;
 }
---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+//2ÁøÆ®¸®
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int *b = &a;
+	int *&c = b;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << &b << endl;
+	cout << c << endl;
+	cout << &c << endl;
+	return 0;
+}
+
+
